872eb2a829f93ff3743cec43b814d1ed35a213d3
dicristomanuel <manuel.dicristo@raise.com>
2017-06-27 15:42:50 -0500

    [utils] separate helper functions

--- src/commands/build.js
+++ src/commands/build.js
@@ -2,77 +2,9 @@
 import documentation from 'documentation';
 import fs from 'fs';
 import path from 'path';
+import { getDirectories, getFiles, hasConfig } from './utils';
 
 const command: string = 'build [input..]';
-const docsYml: string = 'docs.yml';
-const extensions: string[] = ['.js'];
-const TEST_DIRECTORY_REGEX: RegExp = /^__.+__$/;
-
-/**
-* Given a source path, this function returns all existing sub-folders
-* @function directoriesFilter
-*/
-const directoriesFilter: Function = (srcPath: string): Function => (
-  file: string,
-): boolean => fs.statSync(path.join(srcPath, file)).isDirectory();
-
-/**
-* Given a specific folder, returns all the files that have one of the
-* extensions allowed.
-* Current extensions: `['.js']`.
-* @function filesFilter
-*/
-const filesFilter: Function = (srcPath: string): Function => (
-  file: string,
-): boolean => {
-  const currentPath = path.join(srcPath, file);
-  return (
-    fs.statSync(currentPath).isFile() &&
-    extensions.includes(path.extname(currentPath))
-  );
-};
-
-/**
-* Returns the complete path to a specific file.
-* @function filePath
-*/
-const filePath: Function = (srcPath: string): Function => (
-  file: string,
-): string => path.join(srcPath, file);
-
-/**
-* Given a folder path, `getDirectories` returns contained
-* sub-folders that are not dedicated to testing.
-* @function getDirectories
-* @returns array of folders names
-*/
-const getDirectories: Function = (srcPath: string): string[] =>
-  fs
-    .readdirSync(srcPath)
-    .filter(directoriesFilter(srcPath))
-    .filter(dir => !dir.match(TEST_DIRECTORY_REGEX));
-
-/**
-* Given a folder path, `getFiles` returns the contained files that
-* satisfy the rules given by {@link filesFilter}.
-* @function getFiles
-*/
-const getFiles: Function = (srcPath: string): string[] =>
-  fs
-    .readdirSync(srcPath)
-    .filter(filesFilter(srcPath))
-    .map(filePath(srcPath));
-
-/**
-* Returns true if the given folder contains the `docs.yml` file expected in the
-* presence of documentation comments.
-* @function hasConfig
-*/
-const hasConfig: Function = (srcPath: string): boolean =>
-  fs
-    .readdirSync(srcPath)
-    .filter((file: string) => file === docsYml)
-    .length > 0;
 
 /**
 * Passes all the files contained in the folder to `documentation.js`
